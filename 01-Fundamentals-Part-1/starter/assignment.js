bf24ecb05238ebb892f390b8bd1046fbca29a1e9
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-09-02 14:56:57 +0300

    Add and done some assigment tasks

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,15 @@
+/*
+const country = "Ukraine";
+const continent = "Europe";
+let population = 36744636;
+const isIsland = false;
+const language = "ukrainian";
+const description = `${country} is in ${continent}, and it's ${~~(population/1000000)} million people speak ${language}`;
+
+console.log(country, continent, population, isIsland, language);
+console.log(population/2);
+console.log(population++);
+console.log(population > 6000000);
+console.log(population < 30000000);
+console.log(description);
+*/
