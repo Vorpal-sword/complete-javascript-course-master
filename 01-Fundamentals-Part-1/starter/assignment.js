178278e347f62d7d24bcc53297a28e0aad6b919b
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-09-05 13:19:59 +0300

    done all assigment to 1st js part

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -3,8 +3,11 @@ const country = "Ukraine";
 const continent = "Europe";
 let population = 36744636;
 const isIsland = false;
+const isEngSpeakingCountry = false;
+const isPopulationLessThen50M = true;
 const language = "ukrainian";
 const description = `${country} is in ${continent}, and it's ${~~(population/1000000)} million people speak ${language}`;
+const aboveOrBelow = ~~(population/1000000)>33? 'above':'below' 
 
 console.log(country, continent, population, isIsland, language);
 console.log(population/2);
@@ -12,4 +15,47 @@ console.log(population++);
 console.log(population > 6000000);
 console.log(population < 30000000);
 console.log(description);
-*/
+
+if (population > 33000000){
+    console.log("Ukrainian's population is above average");
+} else console.log(`Ukrainian's population is ${33-~~(population/1000000)} below average`);
+// '9' - '5'; //4
+// '19' - '13' + '17'; //617
+// '19' - '13' + 17; // 23
+// '123' < 57; //false
+// 5 + 6 + '4' + 9 - 4 - 2; //1143
+const numNeigbours = Number(prompt('How many neighbour countries does your country have?'));
+if (numNeigbours === 1) {
+    console.log('Only 1 border!');
+} else if(numNeigbours > 1){
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+}
+
+if (isEngSpeakingCountry && isPopulationLessThen50M && !isIsland){
+    console.log(`You should live in ${country}`);
+} else console.log(`${country} does not meet your criteria`);
+
+switch(language){
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default: 
+        console.log('Great language too :D');
+}
+    console.log(`Ukrainian's population is ${aboveOrBelow} average`);
+    */
