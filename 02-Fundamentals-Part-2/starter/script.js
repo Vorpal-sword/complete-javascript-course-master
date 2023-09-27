b5c10d9462d239804d330f4a849c4b5855f9fad1
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-09-27 11:44:29 +0300

    done 1-st loop lecture

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -326,7 +326,6 @@ console.log(max.getSummery());
 
 //Challenge
 //"Max is a 3..-year old learner, and he has a/no driver's license"
-*/
 
 ///////////////////////////////////////
 // Coding Challenge #3
@@ -364,3 +363,22 @@ const john = {
 mark.calcBMI();
 john.calcBMI();
 console.log(`${john.bmi > mark.bmi ? `${john.fullName} BMI (${john.bmi}) is higher than ${mark.fullName} (${mark.bmi})!` : `${mark.fullName} BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})!`}`);
+*/
+///////////////////////////////////////
+// Iteration: The for Loop
+
+// console.log('Lifting weights repetition 1 🏋️‍♀️');
+// console.log('Lifting weights repetition 2 🏋️‍♀️');
+// console.log('Lifting weights repetition 3 🏋️‍♀️');
+// console.log('Lifting weights repetition 4 🏋️‍♀️');
+// console.log('Lifting weights repetition 5 🏋️‍♀️');
+// console.log('Lifting weights repetition 6 🏋️‍♀️');
+// console.log('Lifting weights repetition 7 🏋️‍♀️');
+// console.log('Lifting weights repetition 8 🏋️‍♀️');
+// console.log('Lifting weights repetition 9 🏋️‍♀️');
+// console.log('Lifting weights repetition 10 🏋️‍♀️');
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 30; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
