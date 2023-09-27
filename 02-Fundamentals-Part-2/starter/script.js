4f226bd2b318308f7e004b9f233e43f9f2b8364a
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-09-27 11:40:51 +0300

    done Coding Challenge #3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -285,7 +285,7 @@ console.log(max);
 //Challenge
 // "Max has 3 friends, and his best friend is called Michael"
 console.log(`${max.firstName} has ${max.friends.length} friends, and his best friend is called ${max.friends[0]}`);
-*/
+
 
 ///////////////////////////////////////
 // Object Methods
@@ -326,4 +326,41 @@ console.log(max.getSummery());
 
 //Challenge
 //"Max is a 3..-year old learner, and he has a/no driver's license"
+*/
 
+///////////////////////////////////////
+// Coding Challenge #3
+
+
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+// 1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+// 3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+// TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+// GOOD LUCK 😀
+//
+
+const mark = {
+  fullName: "Mark Miller's",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function(){
+      this.bmi = this.mass/(this.height*this.height);
+      return this.bmi;
+  },
+};
+const john = {
+  fullName: "John Smith's",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function(){
+      this.bmi = this.mass/(this.height*this.height);
+      return this.bmi;
+  },
+};
+mark.calcBMI();
+john.calcBMI();
+console.log(`${john.bmi > mark.bmi ? `${john.fullName} BMI (${john.bmi}) is higher than ${mark.fullName} (${mark.bmi})!` : `${mark.fullName} BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})!`}`);
