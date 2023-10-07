f831ac86a9c423e47dfbbfd9c94c5d225fad9f13
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-10-07 16:02:50 +0300

    done String Methods Practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -46,6 +44,38 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const rows = flights.split('+');
+const delayedItem = '🔴';
+for (const row of rows) {
+  const splitedRow = row.split(';');
+  const [first, from, to, time] = splitedRow;
+  let firstMod = first.replace('_', ' ');
+  if (first.includes('Delayed')) {
+    firstMod = delayedItem + firstMod;
+  }
+  const fromMod = from.slice(0, 3).toUpperCase();
+  const toMod = to.slice(0, 3).toUpperCase();
+  const timeMod = `(${time.replace(':', 'h')})`;
+  const output = `${firstMod} from ${fromMod} to ${toMod} ${timeMod}`.padStart(
+    45,
+    ' '
+  );
+  console.log(output);
+}
+
+/* 
 ///////////////////////////////////////
 // Coding Challenge #4
 
@@ -97,7 +127,7 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(outputs);
   }
 });
-/* 
+
 ///////////////////////////////////////
 // Working With Strings - Part 3
 //Split and Join
