da9554de23689fffba0701646dfb28c2e2e84e7c
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-10-08 12:51:06 +0300

    done Passing Arguments Works

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
-
+/*
+///////////////////////////////////////
+// Default Parameters
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +27,28 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+const flight = 'LH234';
+const max = {
+  name: 'Maxim Suprunenko',
+  passport: 2342342234,
+};
+const checkIn = function (flightNum, passanger) {
+  flightNum = 'LH999';
+  passanger.name = 'Mr. ' + passanger.name;
+  if (passanger.passport === 2342342234) {
+    alert('Check In');
+  } else alert('Wrong passport');
+};
+
+//checkIn(flight, max);
+console.log(flight);
+console.log(max);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+newPassport(max);
+checkIn(flight, max);
