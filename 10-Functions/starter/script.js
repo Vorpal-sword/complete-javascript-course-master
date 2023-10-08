cbc2fdeaa552684f8a0f0460b36e29529c44de17
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-10-08 12:19:06 +0300

    done default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassangers = 1,
+  price = 100 * numPassangers
+) {
+  //ES5
+  //   numPassangers ||= 1;
+  //   price ||= 199;
+  const booking = {
+    flightNum,
+    numPassangers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
