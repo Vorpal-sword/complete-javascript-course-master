0e0f69d96734761f580b471f3f9bc611000e9a03
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-10-10 17:35:18 +0300

    done.forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -117,3 +117,30 @@ console.log(arr[arr.length - 1]);
 console.log(arr.slice(-1)[0]);
 console.log(arr.at(-1));
 */
+///////////////////////////////////////
+// Looping Arrays: forEach
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited${movement}`);
+  } else {
+    console.log(`You withdrew${movement}`);
+  }
+}
+
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited${movement}`);
+  } else {
+    console.log(`You withdrew${movement}`);
+  }
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+
+///////////////////////////////////////
+// forEach With Maps and Sets
