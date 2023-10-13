d1ab011bb4cf93fef563e92298cbe2f89c2a2cc4
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2023-10-13 14:34:50 +0300

    done basic filter method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -250,3 +250,17 @@ const movementsDescriprion = movements.map(
 
 console.log(movementsDescriprion);
 */
+///////////////////////////////////////
+// The filter Method
+
+const deposits = movements.filter(mov => {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
